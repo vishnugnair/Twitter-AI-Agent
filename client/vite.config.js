b7fe088ae2fbbddbabd2dd53fcced1bd60642b12
vishnugnair <119111934+vishnugnair@users.noreply.github.com>
2025-07-22 07:38:34 +0530

    fallbacks added

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,9 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   publicDir: "public", // This should be here
+  server: {
+    historyApiFallback: true, // This fixes the refresh issue
+  },
   build: {
     copyPublicDir: true, // Add this line
   },
